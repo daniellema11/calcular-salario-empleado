f36ce3be9452d444bef413da304b3c450eeb8ab4
daniellema11 <dani_lema_92@hotmail.com>
2025-04-03 16:44:01 -0500

    se definen las variables y se captura info basica

--- src/App.java
+++ src/App.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
-// esto se pone para el scanner o el programa lea la informacion
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in); 
-        // esto se coloca para que el editor capture por teclado es LEER seria SC  que pusimos o el que queramos
+        Scanner sc = new Scanner(System.in);
 
-        int valorHora, cantidadHoras;
+        String nombreEmpleado, docIdentidad, diasDescanso = "";
+        double salarioBruto = 0;
+        double salarioNeto = 0;
+        double deduccionPension = 0;
+        double deduccionSalud = 0;
+        double horasTrabajadas = 0;
+        double valorHora = 0;
+        double bonificacionEmpleado = 0;
+        double valorSalarioMinimo = 1432500;
+        double auxiliotransporte = 200000;
+        double cantidadHorasExtra = 0;
+        double valorHoraExtra = 0;
+        double totalValorHorasExtras = 0;
 
-        valorHora = sc.nextInt();
-        cantidadHoras = sc.nextInt();
-        // se debe colocar luego de la variable, la letra que se le dio como leer sc, 
-        // poner next y lo siguiente es dependiendo de la variable en este caso int, termina con parentesis.
-
-        System.out.println(valorHora*cantidadHoras);
-        sc.close();
-
+        System.out.println("Ingrese el nombre del empleado: ");
+        nombreEmpleado = sc.nextLine();
+        System.out.println("Ingrese el documento de identidad del empleado: ");
+        docIdentidad = sc.nextline();
+        System.out.println("Ingrese los dias de descanso del empleado: (lunes-viernes) ");
+        diasDescanso = sc.nextline();
+        
     }
 }
